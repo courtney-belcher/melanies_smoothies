54e94f4e6b011b3aa7e695f4b31058add3ad7e1c
courtney-belcher <cbelcher819@gmail.com>
2026-02-13 10:42:04 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -42,6 +43,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
-
-import requests
-
